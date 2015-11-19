ee65b10aa24ef21f2719d08cbbe1f5f599331b22
Benjamin Kulas <nazekthetweaker@gmail.com>
2015-11-18 23:06:48 -0600

    First complete version of parsing library
    - Added handling of comments and string literals

--- parsing.py
+++ parsing.py
@@ -1,4 +1,92 @@
+# Arcyou's parsing library. Handles comment removal, string literals, and
+# abstract syntax tree creation.
+
 import errors
+from re import sub, MULTILINE
+from sys import getrecursionlimit, setrecursionlimit
+
+ESCAPES = {'n': '\n',
+           't': '\t',
+           'v': '\v',
+           'a': '\a',
+           'b': '\b',
+           'f': '\f',
+           'r': '\r'}
+
+
+def parse(code):
+    """
+The top-level function for this library, the only one that should be called
+externally. It applies all of the above functions in sequence to some raw
+code that it is passed.
+    """
+    old_rl = getrecursionlimit()
+    setrecursionlimit(code.count('(') + 20) # This should be comfortable
+    stage1 = remove_comments(code)
+    stage2, literals = extract_string_literals(stage1)
+    stage3 = tokenize(stage2)
+    stage4 = clean(stage3)
+    stage5 = insert_string_literals(stage4, literals)
+    setrecursionlimit(old_rl)
+    return stage5
+
+def remove_comments(code):
+    """
+Remove all comments from a program.
+    """
+    return sub(r";.*$", '', code, flags=MULTILINE)
+
+def handle_string_literals(code):
+    """
+Deal with string literals inside an Arcyou program. Also handles Unicode
+escapes, ASCII escapes, and a few other standard ones.
+    """
+    code = code.replace('\x02', '')
+    final = ""
+    in_string = False
+    literals = []
+    pointer = 0
+    index = 0
+    temp = ""
+    while pointer < len(code):
+        char = code[pointer]
+        if in_string:
+            if char == '\\':
+                if code[pointer+1] == 'u':
+                    # Handle Unicode escapes
+                    temp += chr(int(code[pointer+2:pointer+6], 16))
+                    pointer += 5
+                elif code[pointer+1] == 'x':
+                    # Handle \x.. escapes
+                    temp += chr(int(code[pointer+2:pointer+4], 16))
+                    pointer += 3
+                else:
+                    # Handle all other escapes
+                    if code[pointer+1] in ESCAPES:
+                        temp += ESCAPES[code[pointer+1]]
+                    else:
+                        temp += code[pointer+1]
+                    pointer += 1
+            elif char == '\"':
+                if temp in literals:
+                        final += '\x02' + chr(literals.index(temp))
+                else:
+                    literals.append(temp)
+                    if index <= 0xFFFF:
+                        final += '\x02' + chr(index)
+                    else:
+                        errors.ArcError('strmax')
+                in_string = False
+
+            else:
+                temp += char
+        else:
+            if char == '\"':
+                in_string = True
+            else:
+                final += char
+        pointer += 1
+    return final, literals
 
 def tokenize(code):
     """
@@ -64,6 +152,13 @@ tokenize("(eq (+ x 1) y)") -> ['eq',('+','x',1),'y']
         code.append(temp)
     return code
 
-def parse(code):
-    tokens = tokenize(code)
-    return clean(code)
+def insert_string_literals(icode, literals):
+    code = icode[:]
+    i = 0
+    while i < len(code):
+        item = code[i]
+        if isinstance(item, str) and str.startswith('\x02'):
+            code[i] = '"' + literals[ord(item[1])] + '"'
+        elif isinstance(item, list):
+            code[i] = insert_string_literals(item, literals)
+        i += 1
